c7c351bcd459ef081f0bb6178b85c27a2ddb40ba
Sebastian Starosz <staroszsebastian@gmail.com>
2024-11-25 21:42:14 +0100

    Init Cpp rewrite

--- gemsiv/arcball/Demo.c
+++ gemsiv/arcball/Demo.c
@@ -11,6 +11,11 @@
 #include "Body.h"
 #include "Ball.h"
 
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 
 typedef struct {long x, y;} Place;
 
